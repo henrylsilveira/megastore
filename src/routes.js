4bc867d40f34cebe40b6d5fd25ac1dbf47396ba7
henrylsilveira <henrylsilveira@gmail.com>
2020-08-04 09:18:08 -0300

    04/08/20

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const ProductController = require('./app/controllers/ProductController');
 const routes = express.Router();
 
 routes.get('/', function (req, res) {
     return res.render("layout.njk")
 });
+routes.get("/products/:id/edit", ProductController.edit)
+routes.get("/products/create", ProductController.create)
+routes.post("/products", ProductController.post)
+routes.put("/products", ProductController.put)
+routes.delete("/products", ProductController.delete)
+
+
+routes.get("/ads/create", function(req,res){
+    return res.redirect("/products/create")
+})
 
 module.exports = routes;
